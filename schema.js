9bf5abff82af363d3c357733f1ece7a2a35a2adc
Paul <paulkoshy890@gmail.com>
2025-01-08 10:09:12 +0530

    done

---
+++ schema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    type: [String],
+    default: ['user'],
+  },
+  profile: {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+  },
+  lastLogin: {
+    type: Date,
+    default:null,
+  },
+  Timestamps:true,
+});
+
+module.exports = mongoose.model('User', UserSchema);
